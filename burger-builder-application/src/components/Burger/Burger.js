4a189ca015036b13e4ff6e9a4ca04d41c3ee9088
amol <amolp@ec-mobility.biz>
2020-09-27 12:38:40 +0530

    Added Compoents for burger builder

---
+++ burger-builder-application/src/components/Burger/Burger.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import classes from './Burger.css'
+
+import BurgerIngredent from './BurgerIngredient/BurgerIngredent';
+const burger = (props) => {
+    let transformIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredent key={igKey + 1} type={igKey} />;
+            });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+
+    if (transformIngredients.length === 0) {
+        transformIngredients = <p>Please start adding ingredients!</p>;
+    }
+    console.log({ transformIngredients });
+    return (
+
+        < div className={classes.Burger} >
+            <BurgerIngredent type="bread-top" />
+
+            {transformIngredients}
+            <BurgerIngredent type="bread-bottom" />
+        </div >
+    );
+}
+
+
+export default burger;
